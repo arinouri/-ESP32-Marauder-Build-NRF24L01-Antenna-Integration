73f31dc00f9d2423d9265ab3ca5b3c70c2ded772
Arian Nouri <115740177+arinouri@users.noreply.github.com>
2025-05-26 15:02:04 -0400

    tft_test.ino

---
+++ tft_test.ino
@@ -0,0 +1,21 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+
+#define TFT_CS   15
+#define TFT_DC   2
+#define TFT_RST  4
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
+
+void setup() {
+  Serial.begin(115200);
+  tft.begin();
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setTextColor(ILI9341_GREEN);
+  tft.setTextSize(2);
+  tft.setCursor(10, 10);
+  tft.println("Display Test OK");
+}
+
+void loop() {
+}
